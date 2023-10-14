dc50d2e645320429624fdc6eb37512af7e895d14
Michael Tyson <myty@users.noreply.github.com>
2023-10-14 14:23:41 -0400

    remove commented code

--- packages/javascript-core/src/setupTests.ts
+++ packages/javascript-core/src/setupTests.ts
@@ -6,16 +6,6 @@ import {
 } from "@rsm-hcd/javascript-testing";
 import { initializeFactories } from "tests/factories";
 
-// global.console = {
-//     ...console,
-//     // uncomment to ignore a specific log level
-//     log: jest.fn(),
-//     debug: jest.fn(),
-//     info: jest.fn(),
-//     // warn: jest.fn(),
-//     // error: jest.fn(),
-// };
-
 beforeAll(() => {
     const options: FactoryInitializerOptions = {
         define: (name, constructor) => Factory.define(name, constructor),
