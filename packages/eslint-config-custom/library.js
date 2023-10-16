cf1ebb6cfba66bd8c787ff43ede27f5e050f2fb1
Michael Tyson <myty@users.noreply.github.com>
2023-10-16 10:52:29 -0400

    add github issue links to eslint library config

--- packages/eslint-config-custom/library.js
+++ packages/eslint-config-custom/library.js
@@ -33,47 +33,47 @@ module.exports = {
     ignorePatterns: ["node_modules/", "dist/"],
     rules: {
         eqeqeq: ["error", "smart"],
-        "@typescript-eslint/ban-ts-comment": "off",
-        "@typescript-eslint/ban-types": "off",
-        "@typescript-eslint/explicit-function-return-type": "off",
-        "@typescript-eslint/naming-convention": "off",
-        "@typescript-eslint/no-confusing-void-expression": "off",
-        "no-empty-function": "off",
-        "@typescript-eslint/no-empty-function": "off",
-        "@typescript-eslint/no-explicit-any": "off",
-        "@typescript-eslint/no-floating-promises": "off",
-        "@typescript-eslint/no-misused-promises": "off",
-        "@typescript-eslint/no-non-null-assertion": "off",
-        "@typescript-eslint/no-redundant-type-constituents": "off",
-        "no-shadow": "off",
-        "@typescript-eslint/no-shadow": "off",
-        "no-throw-literal": "off",
-        "@typescript-eslint/no-throw-literal": "off",
-        "@typescript-eslint/no-unnecessary-condition": "off",
-        "@typescript-eslint/no-unnecessary-type-constraint": "off",
-        "@typescript-eslint/no-unsafe-argument": "off",
-        "@typescript-eslint/no-unsafe-assignment": "off",
-        "@typescript-eslint/no-unsafe-call": "off",
-        "@typescript-eslint/no-unsafe-member-access": "off",
-        "@typescript-eslint/no-unsafe-return": "off",
-        "@typescript-eslint/require-array-sort-compare": "off",
-        "require-await": "off",
-        "@typescript-eslint/require-await": "off",
-        "@typescript-eslint/unbound-method": "off",
-        "func-names": "off",
-        "import/no-default-export": "off",
-        "import/no-named-as-default-member": "off",
-        "import/no-named-as-default": "off",
-        "new-cap": "off",
-        "no-await-in-loop": "off",
-        "no-console": "off",
-        "no-constant-binary-expression": "off",
-        "no-extend-native": "off",
-        "no-param-reassign": "off",
-        "no-promise-executor-return": "off",
-        "no-prototype-builtins": "off",
-        "prefer-named-capture-group": "off",
-        "prefer-regex-literals": "off",
-        "tsdoc/syntax": "off",
+        "@typescript-eslint/ban-ts-comment": "off", // TODO: https://github.com/rsm-hcd/hcd-javascript/issues/35
+        "@typescript-eslint/ban-types": "off", // TODO: https://github.com/rsm-hcd/hcd-javascript/issues/36
+        "@typescript-eslint/explicit-function-return-type": "off", // TODO: https://github.com/rsm-hcd/hcd-javascript/issues/37
+        "@typescript-eslint/naming-convention": "off", // TODO: https://github.com/rsm-hcd/hcd-javascript/issues/38
+        "@typescript-eslint/no-confusing-void-expression": "off", // TODO: https://github.com/rsm-hcd/hcd-javascript/issues/39
+        "no-empty-function": "off", // TODO: https://github.com/rsm-hcd/hcd-javascript/issues/40
+        "@typescript-eslint/no-empty-function": "off", // TODO: https://github.com/rsm-hcd/hcd-javascript/issues/40
+        "@typescript-eslint/no-explicit-any": "off", // TODO: https://github.com/rsm-hcd/hcd-javascript/issues/61
+        "@typescript-eslint/no-floating-promises": "off", // TODO: https://github.com/rsm-hcd/hcd-javascript/issues/62
+        "@typescript-eslint/no-misused-promises": "off", // TODO: https://github.com/rsm-hcd/hcd-javascript/issues/41
+        "@typescript-eslint/no-non-null-assertion": "off", // TODO: https://github.com/rsm-hcd/hcd-javascript/issues/42
+        "@typescript-eslint/no-redundant-type-constituents": "off", // TODO: https://github.com/rsm-hcd/hcd-javascript/issues/43
+        "no-shadow": "off", // TODO: https://github.com/rsm-hcd/hcd-javascript/issues/45
+        "@typescript-eslint/no-shadow": "off", // TODO: https://github.com/rsm-hcd/hcd-javascript/issues/45
+        "no-throw-literal": "off", // TODO: https://github.com/rsm-hcd/hcd-javascript/issues/46
+        "@typescript-eslint/no-throw-literal": "off", // TODO: https://github.com/rsm-hcd/hcd-javascript/issues/46
+        "@typescript-eslint/no-unnecessary-condition": "off", // TODO: https://github.com/rsm-hcd/hcd-javascript/issues/47
+        "@typescript-eslint/no-unnecessary-type-constraint": "off", // TODO: https://github.com/rsm-hcd/hcd-javascript/issues/48
+        "@typescript-eslint/no-unsafe-argument": "off", // TODO: https://github.com/rsm-hcd/hcd-javascript/issues/49
+        "@typescript-eslint/no-unsafe-assignment": "off", // TODO: https://github.com/rsm-hcd/hcd-javascript/issues/50
+        "@typescript-eslint/no-unsafe-call": "off", // TODO: https://github.com/rsm-hcd/hcd-javascript/issues/51
+        "@typescript-eslint/no-unsafe-member-access": "off", // TODO: https://github.com/rsm-hcd/hcd-javascript/issues/52
+        "@typescript-eslint/no-unsafe-return": "off", // TODO: https://github.com/rsm-hcd/hcd-javascript/issues/53
+        "@typescript-eslint/require-array-sort-compare": "off", // TODO: https://github.com/rsm-hcd/hcd-javascript/issues/53
+        "require-await": "off", // TODO: https://github.com/rsm-hcd/hcd-javascript/issues/55
+        "@typescript-eslint/require-await": "off", // TODO: https://github.com/rsm-hcd/hcd-javascript/issues/55
+        "@typescript-eslint/unbound-method": "off", // TODO: https://github.com/rsm-hcd/hcd-javascript/issues/57
+        "func-names": "off", // TODO: https://github.com/rsm-hcd/hcd-javascript/issues/58
+        "import/no-default-export": "off", // TODO: https://github.com/rsm-hcd/hcd-javascript/issues/59
+        "import/no-named-as-default-member": "off", // TODO: https://github.com/rsm-hcd/hcd-javascript/issues/60
+        "import/no-named-as-default": "off", // TODO: https://github.com/rsm-hcd/hcd-javascript/issues/63
+        "new-cap": "off", // TODO: https://github.com/rsm-hcd/hcd-javascript/issues/64
+        "no-await-in-loop": "off", // TODO: https://github.com/rsm-hcd/hcd-javascript/issues/65
+        "no-console": "off", // TODO: https://github.com/rsm-hcd/hcd-javascript/issues/66
+        "no-constant-binary-expression": "off", // TODO: https://github.com/rsm-hcd/hcd-javascript/issues/67
+        "no-extend-native": "off", // TODO: https://github.com/rsm-hcd/hcd-javascript/issues/68
+        "no-param-reassign": "off", // TODO: https://github.com/rsm-hcd/hcd-javascript/issues/69
+        "no-promise-executor-return": "off", // TODO: https://github.com/rsm-hcd/hcd-javascript/issues/70
+        "no-prototype-builtins": "off", // TODO: https://github.com/rsm-hcd/hcd-javascript/issues/71
+        "prefer-named-capture-group": "off", // TODO: https://github.com/rsm-hcd/hcd-javascript/issues/72
+        "prefer-regex-literals": "off", // TODO: https://github.com/rsm-hcd/hcd-javascript/issues/73
+        "tsdoc/syntax": "off", // TODO: https://github.com/rsm-hcd/hcd-javascript/issues/74
     },
 };
